2e390252aeeb9e43f5bd3182feabf23757feeedc
Chamiya2003 <cchamidu880@gmail.com>
2025-05-26 11:36:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=Ul5kCALZ#2ba3-1vx7haXx-uQ-VcEvD13EFwuHNwbaJOPV-Qa_mE",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
